9c2281465f135274eca1c007d02abb9eeac7458e
Mirco53 <mircoboerm@gmx.de>
2016-05-22 21:07:41 +0200

    no message

---
+++ terrarium/app/src/main/java/terrariumwatch/datasource/login.php
@@ -0,0 +1,31 @@
+<?php
+	 include 'config.php';
+
+	 // Check whether username or password is set from android
+     if(isset($_POST['username']) && isset($_POST['password']))
+     {
+		  // Innitialize Variable
+		  $result = '';
+	   	  $username = $_POST['username'];
+          $password = $_POST['password'];
+
+		  // Query database for row exist or not
+          $sql = 'SELECT time FROM seven_day ORDER BY time DESC LIMIT 1';
+          $stmt = $conn->prepare($sql);
+          //$stmt->bindParam(':username', $username, PDO::PARAM_STR);
+          //$stmt->bindParam(':password', $password, PDO::PARAM_STR);
+          $stmt->execute();
+          if($stmt->rowCount())
+          {
+			 $result = "true";
+          }
+          elseif(!$stmt->rowCount())
+          {
+			  	$result = "false";
+          }
+
+		  // send result back to android
+   		  echo $result;
+  	}
+
+?>
---
+++ terrarium/app/src/main/java/terrariumwatch/terrarium/MainActivity.java
@@ -0,0 +1,262 @@
+package terrariumwatch.terrarium;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    private boolean authenticated = false;
+    private String user, pass;
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem refreshOption = menu.findItem(R.id.action_refresh);
+        MenuItem loginOption = menu.findItem(R.id.action_login);
+        if (authenticated) {
+            loginOption.setEnabled(false);
+            refreshOption.setEnabled(true);
+        } else {
+            refreshOption.setEnabled(true);
+            refreshOption.setEnabled(false);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_login) {
+            final Dialog login = new Dialog(this);
+            login.setContentView(R.layout.login_dialog);
+
+            Button btnLogin = (Button) login.findViewById(R.id.btnLogin);
+            Button btnCancel = (Button) login.findViewById(R.id.btnCancel);
+            final EditText txtUsername = (EditText) login.findViewById(R.id.txtUsername);
+            final EditText txtPassword = (EditText) login.findViewById(R.id.txtPassword);
+
+            btnLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    user = txtUsername.getText().toString();
+                    pass = txtPassword.getText().toString();
+                    new AsyncLogin().execute(user,pass);
+                    login.dismiss();
+                }
+            });
+
+            btnCancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    login.dismiss();
+                }
+            });
+            login.show();
+
+            return true;
+        }
+        else if (id == R.id.action_refresh) {
+            Toast.makeText(MainActivity.this, "Daten werden aktualisiert", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position){
+                case 0:
+                    return OverviewFragment.newInstance();
+                case 1:
+                    return VentFragment.newInstance();
+                case 2:
+                    return LiveFragment.newInstance();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Überblick";
+                case 1:
+                    return "Lüfter";
+                case 2:
+                    return "Live";
+            }
+            return null;
+        }
+    }
+
+    private class AsyncLogin extends AsyncTask<String, String, String> {
+        ProgressDialog pdLoading = new ProgressDialog(MainActivity.this);
+        HttpURLConnection conn;
+        URL url = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pdLoading.setMessage("\tLoading...");
+            pdLoading.setCancelable(false);
+            pdLoading.show();
+
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                url = new URL("login.php");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return "exception";
+            }
+            try {
+                // Setup HttpURLConnection class to send and receive data from php and mysql
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+
+                // setDoInput and setDoOutput method depict handling of both send and receive
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                // Append parameters to URL
+                Uri.Builder builder = new Uri.Builder()
+                        .appendQueryParameter("username", params[0])
+                        .appendQueryParameter("password", params[1]);
+                String query = builder.build().getEncodedQuery();
+
+                // Open connection for sending data
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+                conn.connect();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+                return "exception";
+            }
+            try {
+                int response_code = conn.getResponseCode();
+                // Check if successful connection made
+                if (response_code == HttpURLConnection.HTTP_OK) {
+                    // Read data sent from server
+                    InputStream input = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+                    // Pass data to onPostExecute method
+                    return(result.toString());
+                } else{
+                    return("unsuccessful");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "exception";
+            } finally {
+                conn.disconnect();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //this method will be running on UI thread
+            pdLoading.dismiss();
+            if(result.equalsIgnoreCase("true")) {
+                Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+            } else if (result.equalsIgnoreCase("false")){
+                // If username and password does not match display a error message
+                Toast.makeText(MainActivity.this, "Invalid user or password", Toast.LENGTH_SHORT);
+            } else if (result.equalsIgnoreCase("exception") || result.equalsIgnoreCase("unsuccessful")) {
+                Toast.makeText(MainActivity.this, "OOPs! Something went wrong. Connection Problem.", Toast.LENGTH_SHORT);
+            }
+        }
+    }
+}
